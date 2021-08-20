673f3d53536a18df6becc80f48e44c95d5f29aac
Deniorrr <49251639+Deniorrr@users.noreply.github.com>
2021-08-20 02:15:35 +0200

    added api reader, changed static sass colors to variables

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.sass']
+  styleUrls: ['./projects.component.sass'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ProjectsComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    //api reader
+    let request = new XMLHttpRequest()
+
+    // Open a new connection, using the GET request on the URL endpoint
+    request.open('GET', 'https://api.github.com/users/Deniorrr/repos', true)
+
+    request.onload = function () {
+      // Begin accessing JSON data here
+      var data = JSON.parse(this.response)
+      data.forEach((repo) => {
+        // Log each repo's title
+        let image_link = `https://raw.githubusercontent.com/Deniorrr/${repo.name}/${repo.default_branch}/portfolio_miniature.png`
+        create_tile(image_link, repo.name, repo.description, repo.language)
+      })
+    }
+    // Send request
+    request.send()
+
   }
 
+}
+
+function create_tile(zdjecie, tytul, opis, language) {
+  let tile = ""
+  tile += `<div class='tile'><img class='preview' src='${zdjecie}'><h1>${tytul}</h1><p>${opis}</p><p>language: ${language}</p></div>`
+  document.getElementById("projects_scraped").innerHTML += tile
 }
