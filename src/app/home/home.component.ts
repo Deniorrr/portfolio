8fdc5df0771d2b01bae17022afadf979a4dac279
Deniorrr <49251639+Deniorrr@users.noreply.github.com>
2021-09-16 00:45:16 +0200

    added main page animations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,13 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let i = 0
+    let animation = setInterval(() => {
+      document.getElementsByClassName("about_thread")[i].classList.add("animate_about")
+      if (i == 2) clearInterval(animation)
+      i += 1
+    }, 150)
+    document.getElementsByClassName("about_thread")[0].classList.add("animate_about")
   }
 
 }
